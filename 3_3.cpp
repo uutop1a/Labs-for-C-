da6e68c9be29f7c9dec118e6d47993de787bf105
uutop1a <kirillkopytov13@gmail.com>
2025-11-25 23:50:29 +0500

    First add

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int numLen(const string& x) {
+    return x.length();
+}
+
+int main() {
+    string y;
+    cout << "Enter x:";
+    cin >> y;
+    cout << numLen(y) << endl;
+    return 0;
+}
